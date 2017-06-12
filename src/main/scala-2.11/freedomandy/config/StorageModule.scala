786192472f35391daee76e23f71c145a332db137
freedomandy <freedomandy.huang@gmail.com>
2017-06-12 23:36:50 +0800

    Create StorageModule.scala
    Assign a specified config file

--- src/main/scala-2.11/freedomandy/config/StorageModule.scala
+++ src/main/scala-2.11/freedomandy/config/StorageModule.scala
@@ -6,7 +6,7 @@ import com.typesafe.config.ConfigFactory
   * Created by andy on 05/06/2017.
   */
 object StorageModule {
-  val conf = ConfigFactory.load()
+  val conf = ConfigFactory.load("storage-env.conf")
   val defaultConfiguration = DefaultConfiguration(conf.getString("AzureblobService.storage.name"),
     conf.getString("AzureblobService.storage.key"))
 
